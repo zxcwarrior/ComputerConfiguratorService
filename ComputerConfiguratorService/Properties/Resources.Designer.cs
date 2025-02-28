118804863443d685f2f7d739300d6ad50f7b9013
123 <123@jkl.com>
2025-02-28 14:02:18 +1000

    add icon

--- ComputerConfiguratorService/Properties/Resources.Designer.cs
+++ ComputerConfiguratorService/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace ComputerConfiguratorService.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
